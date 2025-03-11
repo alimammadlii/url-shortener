55369e751db56767a1846575cab83e0a4f936688
alimammadlii <ali.mammadli@bil.omu.edu.tr>
2025-03-11 22:27:03 +0300

    visualise shortener

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
 
 mongoose
   .connect(process.env.MONGO_URI)
