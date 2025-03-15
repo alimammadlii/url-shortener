9f5e4489a583d213a3aa1a5da03f3394908edfec
alimammadlii <ali.mammadli@bil.omu.edu.tr>
2025-03-15 22:13:56 +0300

    Add rate limiting middleware

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import express from "express";
-import Url from "./models/shortUrl.js";
 import mongoose from "mongoose";
-import { nanoid } from "nanoid";
 import dotenv from "dotenv";
+import urlRoutes from "./routes/urlRoutes";
 
 dotenv.config();
 const app = express();
@@ -13,51 +12,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
 mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
+.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err));
 
-
-app.get("/", (req, res) => {
-    res.send("Welcome to url shortener");
-})
-
-
-app.get("/:shortId", async (req, res) => {
-    const { shortId } = req.params;
-  
-    try {
-      const urlEntry = await Url.findOne({ shortId });
-  
-      if (urlEntry) {
-        urlEntry.clicks += 1; // Increment click count
-        await urlEntry.save();
-  
-        return res.redirect(urlEntry.url); // Redirect to original URL
-      } else {
-        return res.status(404).json({ error: "❌ Short URL not found" });
-      }
-    } catch (error) {
-      res.status(500).json({ error: "❌ Server error" });
-    }
-});
-
-app.post("/shorten", async (req, res) => {
-    const { url } = req.body;
-    console.log(url);
-    if (!url) {
-        return res.status(400).json({error: "URL is required"});
-    }
-    try {
-        const shortId = nanoid(6);
-        const newUrl = new Url({url, shortId})
-        await newUrl.save();
-        res.json({ shortUrl: `https://url-shortener-production-9b70.up.railway.app/${shortId}`});
-    } catch(error) {
-        res.status(500).json({error: "Server error"});
-    }
-})
-
+app.use("/", urlRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
